9733748d815a314e4caa27e7649fa56c6b808977
a13pro <anna@prodan.pro>
2022-04-11 23:32:44 +0300

    HomeWork l.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int clientOS = 0; // 0 — iOS, 1 — Android
+        // задача 1
+        int clientOS = 1; // 0 — iOS, 1 — Android
         if (clientOS == 0) {
             System.out.println("Install iOS version of the app using the link below");
-        }
-        if (clientOS == 1) {
+        } else {
             System.out.println("Install Android version of the app using the link below");
+        }
+
+        // задача 2
+
+        int clientDeviceYear = 2015;
+        if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Install the light version of  Android app using the link below ");
+            } else {
+                System.out.println("Install Android version of the app using the link below");
+            }
+        } else if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Install the light version of  iOS app using the link below");
+            } else {
+                System.out.println("Install iOS version of the app using the link below");
+            }
+        }
+        // задача 3
+        int year = 2021;
+        if (year % 4 != 0) {
+            System.out.println("It`s a normal year");
+        }else if (year % 400 != 0 && year % 100 == 0) {
+            System.out.println("It`s a normal year");
+        } else {
+            System.out.println("It`s a leap year");
         }
     }
 }
+
