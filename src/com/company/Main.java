27346be6876b812390f9d0c95f85b95270c66e0e
a13pro <anna@prodan.pro>
2022-04-12 22:09:14 +0300

    HomeWork l.2 last commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,10 +31,51 @@ public class Main {
         int year = 2021;
         if (year % 4 != 0) {
             System.out.println("It`s a normal year");
-        }else if (year % 400 != 0 && year % 100 == 0) {
+        } else if (year % 400 != 0 && year % 100 == 0) {
             System.out.println("It`s a normal year");
         } else {
             System.out.println("It`s a leap year");
+        }
+
+        // Задача 4
+
+        double deliveryDistance = 95;
+        double daysForDelivery = 0;
+        double additionalDays = (deliveryDistance - 20) / 40;
+        double addDaysRoundUp = Math.ceil(additionalDays);
+        if (deliveryDistance > 20) {
+            daysForDelivery = 1 + addDaysRoundUp;
+            System.out.println("Delivery will take " + daysForDelivery);
+        } else {
+            System.out.println("Delivery will take 1 day");
+        }
+
+        // задача 5
+
+        var monthNumber = 11;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("It`s Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("It`s Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("It`s Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("It`s Autumn");
+                break;
+            default:
+                System.out.println("There is no such month");
         }
     }
 }
